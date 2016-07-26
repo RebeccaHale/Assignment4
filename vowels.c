78ff917fc2dc44c42c43a037dae1e8d452f80e1a
Rebecca Hale <rkh2128@coumbia.edu>
2016-07-26 15:20:19 -0400

    vowels revised

--- vowels.c
+++ vowels.c
@@ -9,7 +9,7 @@ int main(){
 	char temp[100];
 	int count = 0;
 	for(i=0; i<strlen(s)-1; i++){
-		if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i] != 'o' && s[i]!='u'){
+		if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i] != 'o' && s[i]!='u' && s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U'){
 			temp[count]=s[i];
 			count++;
 		}
